d0cbf83eb36dd34b8b8aba42bda7a1f670b3dfbc
Salim-rashid1 <166801225+Salim-rashid1@users.noreply.github.com>
2024-10-01 08:47:09 +0300

    EVN_average_customer added

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,8 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT `EVN_average_customer_waiting_time_every_1_hour`
+ON SCHEDULE EVERY 1 HOUR
+DO
+  INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)
+  SELECT IFNULL(AVG(waiting_time_minutes), 0) AS average_waiting_time
+  FROM customer_service_ticket
+  WHERE ticket_created_time >= NOW() - INTERVAL 1 HOUR;
